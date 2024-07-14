240532c21b25fb56cb29502ce1b5c318facb0cfb
vaishnavi2695 <vaishnavi.ramagiri15@gmail.com>
2024-07-14 22:47:50 +0530

    V

--- src/containers/Home/Home.jsx
+++ src/containers/Home/Home.jsx
@@ -3,7 +3,12 @@ import "./Home.css";
 function Home() {
   return (
     <div>
-      <h1>Hi I Am R.Vaishnavi</h1>
+      <h1>Hi </h1>
+      <p class="name">I Am R.Vaishnavi</p>
+      <p class="hi">Aspiring [Web Developer].</p>
+      <p class="hlo">Recent Graduate Seeking Growth Oppurtunities</p>
+      <br></br>
+      <hr></hr>
     </div>
   )
 }
